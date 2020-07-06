1868e0c0a3ed78dc1643039dbd460b737970b087
IsisCunha <37351239+IsisCunha@users.noreply.github.com>
2020-07-06 19:48:47 -0300

    Exercícios

---
+++ ex06.js
@@ -0,0 +1,13 @@
+//06)​ Elabore duas funções que recebem três parâmetros: capital inicial, taxa de juros e tempo de aplicação.
+//A primeira função retornará o montante da aplicação financeira sob o regime de juros simples e 
+//a segunda retornará o valor da aplicação sob o regime de juros compostos
+
+function jurosSimples(capital, taxa, tempo){
+    const jurosMes = (capital * taxa)   / 100
+    const valorAplicacao = (jurosMes * tempo) + capital
+    console.log('R$',valorAplicacao)
+
+
+}
+
+jurosSimples(500, 3, 8)
